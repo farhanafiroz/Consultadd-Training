8733cf34e0d2dfe7467f481f44f3e911733dedbd
farhanaonna <farhanafiroz@gmail.com>
2020-05-12 21:30:18 -0700

    Task6:Question number one is completed.

--- Q1.py
+++ Q1.py
@@ -1,4 +1,8 @@
-a=3
-b=5
-print (a+b)
-print ("Hello World")
+#1.Write a program to Python find the values which is not divisible 3 but is should be a multiple of 7.
+# Make sure to use only higher order function.
+
+def multiple(x):
+	return True if x % 3 != 0 and x % 7 ==0 else False
+
+print(multiple(20))
+print(multiple(7))
